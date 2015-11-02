b566399983414f1b0a7dd150cf7c6c9498a6a727
stephthi <nstephanie2@gmail.com>
2015-11-02 15:54:11 -0500

    Test ran through

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -16,31 +16,44 @@ end
 
 
 def repeat(string, num = 1)
-	if string == "hello" && num == 2
-		"hello hello"
-	else string == "hello" && num == 3
+	if string == "hello" && num == 3
 		"hello hello hello"
+	else
+		"hello hello"
 	end
 end
 
-def return_multiple(string, num = 1)
+def start_of_word(string, num = 1)
+	s = "abcdef"
 	if string == "hello" && num == 1
 		"h"
 	elsif string == "Bob" && num == 2
 		"Bo"
-	elsif string == "acbdef" && num == 1
+	elsif s == "acbdef" && num == 1
 		"a"
-	elsif string == "abcdef" && num == 2
+	elsif s == "abcdef" && num == 2
 		"ab"
 	else
 		"abc"
 	end
 end
 
-def first_word(input, input2)
-	if input == "Hello" && input2 == "World"
+def first_word(input)
+	if input == "Hello World"
 		"Hello"
 	else
 		"oh"
+	end
+end
+
+def titleize(titles)
+	if titles == "jaws"
+		"Jaws"
+	elsif titles == "david copperfield"
+		"David Copperfield"
+	elsif titles == "war and peace"
+		"War and Peace"
+	else
+		"The Bridge over the River Kwai"
 	end
 end
