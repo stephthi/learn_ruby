5ead4b11768ad94561364029beb3bf6fedec2c9e
stephthi <nstephanie2@gmail.com>
2015-11-02 15:18:59 -0500

    Completed calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,27 +1,12 @@
 def add(x, y)
-	if x == 0 && y == 0
-		0
-	elsif x == 2 && y == 2
-		4
-	elsif x == 2 && y == 6
-		8
-	end
+	x + y
 end
 
 def subtract(x, y)
-	if x == 10 && y == 4
-		6
-	end
+	x - y
 end
 
 def sum(sum_array)
-	if sum_array == []
-		0
-	elsif sum_array == [7]
-		7
-	elsif sum_array == [7, 11]
-		18
-	else
-		25
+	sum_array.inject(0) do |sum, number| sum + number
 	end
 end
