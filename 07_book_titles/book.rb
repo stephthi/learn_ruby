2d45f90455b3c58f307d28b9859d0fd09af1bdd4
stephthi <nstephanie2@gmail.com>
2015-11-02 20:57:03 -0500

    Created class and methods

---
+++ 07_book_titles/book.rb
@@ -0,0 +1,19 @@
+class Book
+	attr_reader :title
+
+	def title=(string)
+		@title = titlelize(string)
+	end
+
+	def titlelize(title)
+		title.capitalize!
+		lowercase = ["and", "over", "the", "a", "in"]
+		title.split.map { |word|
+		if lowercase.include?(word)
+			word
+		else
+			word.capitalize
+		end
+		}.join(" ")
+	end
+end
